d9cf0fdf232a0d5e3c4e9461c3fd2321ff498890
doni-robert <irobertndungu@gmail.com>
2023-11-16 18:06:54 +0300

    add files

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+from flask import (
+        Flask, jsonify, request, abort, make_response, redirect, url_for)
+from auth import Auth
+from typing import Dict
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify()
+
+
+@app.route('/users', methods=['POST'])
+def user() -> Dict[str, str]:
+    """
+    Registers a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({
+            "email": email,
+            "message": "user created"
+            })
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> Dict[str, str]:
+    """
+    Creates a new session for the user and stores the session ID as
+    a cookie
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+
+        response = make_response(jsonify({
+            "email": email,
+            "message": "logged in"
+            }))
+        response.set_cookie("session_id", session_id)
+
+        return response
+    else:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> Dict[str, str]:
+    """
+    Logs out a user
+    """
+    session_id = request.cookie.get('session_id')
+    user = Auth.get_user_from_id(session_id)
+    if user:
+        Auth.destroy_session(user.id)
+        return redirect(url_for('/'))
+    abort(403)
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> Dict[str, str]:
+    """
+    Retrieves a user's profile
+    """
+    session_id = request.cookie.get('session_id')
+    user = Auth.get_user_from_id(session_id)
+    if user:
+        response = make_response(jsonify({
+            "email": user.email
+            }), 200)
+        return response
+    abort(403)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+""" The auth module """
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+import uuid
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Returns a hashed passwor in bytes
+    """
+    bytes_pwd = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash_pwd = bcrypt.hashpw(bytes_pwd, salt)
+
+    return hash_pwd
+
+
+def _generate_uuid() -> str:
+    """
+    Returns a string representation of a new UUID
+    """
+    return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a user
+        """
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hash_pwd = _hash_password(password)
+            user = self._db.add_user(email, hash_pwd)
+            return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates a user's credentials
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            pwd_bytes = password.encode('utf-8')
+
+            if bcrypt.checkpw(pwd_bytes, user.hashed_password):
+                return True
+            else:
+                return False
+        except (NoResultFound, InvalidRequestError) as e:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """
+        creates a session for its user
+
+        Return:
+            Returns the session id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            new_uuid = _generate_uuid()
+            self._db.update_user(user.id, session_id=new_uuid)
+
+            return new_uuid
+        except Exception:
+            return None
+
+    def get_user_from_id(self, session_id: str) -> User:
+        """
+        Retrieves a user using the session_id
+        """
+        user = self._db.find_user_by(session_id=session_id)
+        if user:
+            return user
+        return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroys a user's session
+        """
+        user = self._db.find_user_by(id=user_id)
+        self._db.update_user(user, session_id, None)
+
+        return None
