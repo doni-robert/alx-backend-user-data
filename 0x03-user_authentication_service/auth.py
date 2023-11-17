1ddf5a52621ba6767030f997efaaf529bc6a08e7
doni-robert <irobertndungu@gmail.com>
2023-11-17 10:19:28 +0300

    correct function name

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,7 +76,7 @@ class Auth:
         except Exception:
             return None
 
-    def get_user_from_id(self, session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """
         Retrieves a user using the session_id
         """
@@ -84,8 +84,7 @@ class Auth:
             user = self._db.find_user_by(session_id=session_id)
             if user:
                 return user
-        else
-            return None
+        return None
 
     def destroy_session(self, user_id: int) -> None:
         """
