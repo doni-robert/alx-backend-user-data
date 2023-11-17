ebaa128ea09d69393eef184fa6dea58049f40bfe
doni-robert <irobertndungu@gmail.com>
2023-11-17 10:39:24 +0300

    correct use of update_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,5 +94,4 @@ class Auth:
         """
         if user_id is None:
             return None
-        user = self._db.find_user_by(id=user_id)
-        self._db.update_user(user, session_id, None)
+        self._db.update_user(user_id, session_id=None)
