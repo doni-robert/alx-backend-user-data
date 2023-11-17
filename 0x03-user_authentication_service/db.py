eac44e9bb0d22ec23c30241d7aa8ecd58fcdba08
doni-robert <irobertndungu@gmail.com>
2023-11-17 09:31:15 +0300

    correct exception

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -71,5 +71,5 @@ class DB:
                 setattr(user, key, value)
                 self._session.commit()
                 return None
-        except ValueError:
+        except Exception
             raise ValueError
